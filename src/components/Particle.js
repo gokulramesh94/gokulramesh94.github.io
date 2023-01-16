78fef5f483e27e1f2325a40dcae240a0548b57d9
Gokul <gokulpulari@gmail.com>
2023-01-16 12:25:48 -0800

    tsparticles fix.

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 
 function Particle() {
+  const particlesInit = async (main) => {
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
   return (
     <Particles
-      params={{
+      id="tsparticles"
+      init={particlesInit}
+      options={{
         particles: {
           number: {
             value: 160,
